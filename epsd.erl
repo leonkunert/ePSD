dcd041918edd291e96546b01c6f6a35cd856370d
Leon Kunert <leonkunert@gmail.com>
2013-09-01 22:59:55 +0200

    Added Version Check, Reserved Check and Channels Check.

--- epsd.erl
+++ epsd.erl
@@ -1,14 +1,14 @@
 -module(epsd).
--export([epsd/1, read_header/1, read_signature/1]).
+-export([epsd/1]).
 
 epsd(Path) ->
     read_header(file:read_file(Path)).
 
 read_header({ok,Binary}) ->
-    read_signature(binary:part(Binary, 0, 4));
-    %% Siganture
-    %% Version
-    %% Reserved
+    read_signature(binary:part(Binary, 0, 4)),
+    read_version(binary:part(Binary, 4, 2)),
+    read_reserved(binary:part(Binary, 6, 6)),
+    read_channels(binary:part(Binary, 12, 2));
     %% Channels
     %% Height
     %% Width
@@ -16,12 +16,40 @@ read_header({ok,Binary}) ->
     %% Color Mode
 
 read_header({error, Reason}) ->
-    io:format("Error").
+    io:format("Error: ~p", Reason).
+
+%%---- Read Signature ----%%
 
 %% Is a PSD
 read_signature(<<"8BPS">>) ->
+    io:format("Is PSD ~n... correct Signature"),
     {ok};
 
 %% Not a PSD
 read_signature(_) ->
+    io:format("Not a PSD: Signature"),
     {error, "Not a PSD"}.
+
+%%---- Read Version ----%%
+
+read_version(<<0,1>>) ->
+    io:format("~n... correct Version"),
+    {ok};
+
+read_version(_) ->
+    {error, unkownVersion}.
+
+%%---- Read Reserved ----%%
+
+read_reserved(<<0,0,0,0,0,0>>) ->
+    io:format("~n... Correct Reserved"),
+    {ok};
+
+read_reserved(_) ->
+    {error, unkown}.
+
+%%---- Read Channels ----%%
+
+read_channels(Channels) ->
+    io:format("~n... Nummber of Channels: ~p", Channels),
+    {ok}.
