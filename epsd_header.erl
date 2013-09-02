2a3fbfb38987f5450f5020de8f376a944ce0835d
Leon Kunert <leonkunert@gmail.com>
2013-09-02 02:25:17 +0200

    Added Height and Width Checkers.

--- epsd_header.erl
+++ epsd_header.erl
@@ -6,8 +6,8 @@ read_header({ok,Binary}) ->
     read_version(binary:part(Binary, 4, 2)),
     read_reserved(binary:part(Binary, 6, 6)),
     read_channels(binary:part(Binary, 12, 2)),
-    read_height(binary:part(Binary, 14, 4));
-    %% Width
+    read_height(binary:part(Binary, 14, 4)),
+    read_width(binary:part(Binary, 18, 4));
     %% Depth
     %% Color Mode
 
@@ -19,42 +19,48 @@ read_header({error, Reason}) ->
 %% Is a PSD
 read_signature(<<"8BPS">>) ->
     io:format("Is PSD ~n... correct Signature"),
-    {ok};
+    {ok, signature};
 
 %% Not a PSD
 read_signature(_) ->
     io:format("Not a PSD: Signature"),
-    {error, "Not a PSD"}.
+    {error, signature "Not a PSD"}.
 
 %%---- Read Version ----%%
 
 read_version(<<0,1>>) ->
     io:format("~n... correct Version"),
-    {ok};
+    {ok, version};
 
 read_version(_) ->
-    {error, unkownVersion}.
+    {error, version, unkownVersion}.
 
 %%---- Read Reserved ----%%
 
 read_reserved(<<0,0,0,0,0,0>>) ->
     io:format("~n... Correct Reserved"),
-    {ok};
+    {ok, reserved};
 
 read_reserved(_) ->
-    {error, unkown}.
+    {error, reserved, unkown}.
 
 %%---- Read Channels ----%%
 
 read_channels(Binary) ->
     [_ | Channels] = binary_to_list(Binary),
     io:format("~n... Nummber of Channels: ~p", Channels),
-    {ok}.
+    {ok, channels, Channels}.
 
 %%---- Read Height ----%%
 
 read_height(Binary) ->
-    Binary.
-    % [_ | Channels] = binary_to_list(Binary),
-    % io:format("~n... Nummber of Channels: ~p", Channels),
-    % {ok}.
+    <<Data:32/integer-unsigned-big>> = Binary,
+    io:format("~n... Height is: "++ integer_to_list(Data)++ " px"),
+    {ok, heigth, Data}.
+
+%%---- Read Width ----%%
+
+read_width(Binary) ->
+    <<Data:32/integer-unsigned-big>> = Binary,
+    io:format("~n... Width is: "++ integer_to_list(Data)++ " px"),
+    {ok, width, Data}.
