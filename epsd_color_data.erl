a39ecfd62e5d63031deb6280d03e97c0017381a7
Leon Kunert <leonkunert@gmail.com>
2013-09-02 03:50:13 +0200

    Finished Color Data Modul

--- epsd_color_data.erl
+++ epsd_color_data.erl
@@ -1,7 +1,22 @@
 -module(epsd_color_data).
 -export([read_color_data/1]).
 
-read_color_data(Binary) ->
+read_color_data({ok, Binary}) ->
     %% Calculating Length
     %% Data
-    io:format("~n Starting with Color Mode Data Section ...").
+    io:format("~nStarting with Color Data ..."),
+    read_lenght(binary:part(Binary, 26, 4));
+
+read_color_data({error, Reason}) ->
+    io:format("Error: ~p", Reason).
+
+%%---- Read Lenght ----%%
+
+read_lenght(Binary) ->
+    <<Data:32/integer-unsigned-big>> = Binary,
+    if  Data =:= 0 ->
+            io:format("~n... No Color Data found.");
+        Data > 0 ->
+            %% Still needs to deal with Color Data
+            io:format("~n... Color Data length: "++ integer_to_list(Data))
+    end.
